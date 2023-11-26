8f79b2bb8d97cb54d39cf2599305e85da19da520
ioanam15 <ioana.m2004@gmail.com>
2023-11-26 15:45:01 +0200

    mesaj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,40 @@
+import Navbar from 'react-bootstrap'
 import logo from './logo.png';
 import insta from './mdi_instagram.png'
 import fb from './uil_facebook.png'
 import twitch from './mdi_twitch.png'
+import ex from './ph_x-fillex.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header classname=".App-navbar">
-        <p>
-        <img src={logo} className="App-logo" alt="logo" />
-        </p>
-        <p>
-        Login
-      </p>
-      <p>
-        Register
-      </p>
-      </header>
+      
+      <nav className="App-navbar" >
+        <img src={logo} className='App-logo' alt=""/>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a className='App-login'>Login</a>
+          <a className='App-register'>Register</a>
+        </div>
+        </div>
+      </nav>
+
       <div className="App-header">
-    
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        <a className="App-popup">
+          <img src={ex} className="App-ics" alt="ex" />
+          <a className='App-pp-log'>Login</a>
+          <a className="App-pp-buton">Login</a>
         </a>
       </div>
+      </div>
+
       <footer className="App-footer">
         <p>
         <img src={insta} alt="insta" />
         <img src={fb} alt="fb" />
         <img src={twitch} alt="twitch" />
-          incercare
         </p>
       </footer>
     </div>
